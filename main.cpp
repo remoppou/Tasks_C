910db409e2007c1cc83dcbb1ae66a13c592a892e
remoppou <voronezhskij_n_m@sc.vsu.ru>
2022-03-24 17:51:57 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 //10) Описать структуру, содержащую информацию о деятельности
@@ -11,7 +12,7 @@ using namespace std;
 //придумать самостоятельно.
 
 struct museum{
-    const char *nameMuseum;
+    char *nameMuseum;
     float coast; //price
     int timeOfWork[2];//1-open;2-close
     const char *adress;
@@ -22,10 +23,11 @@ struct museum{
     workers workers[5];
     struct halls{
         struct exhibits{
-            const char *nameEx;
+            string nameEx;
             int age;
             int hall;
         };
+        exhibits exhibits;
     };
     halls halls[3];
     struct excursions{
@@ -39,12 +41,16 @@ struct museum{
 int main() {
     museum mus;
     //info about museum
+    cout << "Info about museum \n";
     mus.nameMuseum = "Voronezh Museum";
     mus.coast = 100;
     mus.timeOfWork[0] = 8;
     mus.timeOfWork[1] = 14;
-    mus.adress = "Plekhanovskaya str., 29";
+    mus.adress = "Plekhanovskaya str.,29";
+    cout << mus.nameMuseum << ", Price:" <<mus.coast << ", Open:" << mus.timeOfWork[0] << ", Close:" << mus.timeOfWork[1] << ", Adress:" << mus.adress << "\n";
+    cout << "\n";
     //workers
+    cout << "Info about workers \n";
     mus.workers[0].name = "Evgeny";
     mus.workers[1].name = "Olga";
     mus.workers[2].name = "Arseniy";
@@ -55,12 +61,47 @@ int main() {
     mus.workers[2].age = 48;
     mus.workers[3].age = 57;
     mus.workers[4].age = 29;
+    for (int w = 0; w < 5; w++) {
+        cout << mus.workers[w].name << ", Age:" << mus.workers[w].age << "\n";
+    }
+    cout << "\n";
+    cout << "Info about halls \n";
     //halls
-
+    string str;
+    int size = 0;
+    ifstream in("D:\\C++Projects\\C_Task4_var10\\input.txt");
+    if(!in.is_open()) {
+        cout << "Error of open input.txt" << endl;
+    } else {
+        int i = 0;
+        while (!in.eof()) {
+            string a;
+            int age;
+            int hall;
+            getline(in , str);
+            in >> a >> age >> hall;
+            switch (hall) {
+                case 1:
+                    in >> mus.halls[0].exhibits.nameEx >> mus.halls[0].exhibits.age >> mus.halls[0].exhibits.age;
+                case 2 :
+                    in >> mus.halls[1].exhibits.nameEx >> mus.halls[1].exhibits.age >> mus.halls[1].exhibits.hall;
+                case 3 :
+                    in >> mus.halls[2].exhibits.nameEx >> mus.halls[2].exhibits.age >> mus.halls[2].exhibits.hall;
+            }
+            i++;
+            size++;
+        }
+    }
+    in.close();
+    cout << "\n";
     //excursion
+    cout << "Info about excursions \n";
     mus.excursions[0].nameOfExc = "History";
     mus.excursions[0].price = 100;
     mus.excursions[1].nameOfExc = "OtherName";
     mus.excursions[1].price = 400;
+    for (int e = 0; e < 2; e++) {
+        cout << mus.excursions[e].nameOfExc << ", Price:" << mus.excursions[e].price << "\n";
+    }
     return 0;
 }

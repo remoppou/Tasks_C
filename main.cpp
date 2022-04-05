75edd71d502cde24c0b05be8dd0cd1912a1ebe32
remoppou <voronezhskij_n_m@sc.vsu.ru>
2022-04-05 12:47:25 +0300

    Friend function

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ using namespace std;
         двух множеств. Реализовать перегрузку оператора разность (-) для
         формирования разности множеств. Предложить и реализовать
         дружественную функцию для данного класса.
-        6. Перегрузку оператора разность(-);
-        7. Дружественная функция; */
+        6. Перегрузку оператора разность(-); */
 
 class Set {
+    friend void friendFunction(Set &, Set&);
 private:
     int *array;
     int sizeArray;
@@ -70,16 +70,14 @@ public:
         }
     }
 
-    friend Set operator- (const Set &set1, const Set &set2);
-
     ~Set() {
         delete[] array;
         sizeArray = 0;
     }
 };
 
-Set operator- (const Set &set1, const Set &set2) {
-    return Set(set1 - set2);
+void friendFunction(Set& set1, Set& set2) {
+    cout << "Size first set: " << set1.numElements << "(You can add: " << set1.sizeArray << ")" << "; Size second set: " << set2.numElements << "(You can add: " << set2.sizeArray << ")."<< endl;
 }
 
 void intersection(Set* set1, Set* set2) {
@@ -139,7 +137,7 @@ int main() {
     Set set1(sizeFirstSet);
     Set set2 (sizeSecondSet);
     while (work) {
-        cout << "Choose action: \n" << "1. Add element (while not end); \n" << "2. Remove element; \n" << "3. Change element; \n" << "4. Intersection; \n" << "5. - ;\n" <<  "6. Output;\n " << "7. Exit." << endl;
+        cout << "Choose action: \n" << "1. Add element (while not end); \n" << "2. Remove element; \n" << "3. Change element; \n" << "4. Intersection; \n" << "5. - ;\n" <<  "6. Output;\n " << "7. Info about sets;\n"<< "8. Exit." << endl;
         cin >> choose;
         int ch;
         int a;
@@ -192,7 +190,7 @@ int main() {
                 intersection(&set1, &set2);
                 break;
             case 5:
-                set1-set2;
+
                 break;
             case 6:  //Output(work)
                 cout << "Choose set (1 or 2) :";
@@ -205,7 +203,10 @@ int main() {
                     set2.output();
                 } else cout << "No Set!!!";
                 break;
-            case 7:     //exit(work)
+            case 7:     //friend function(work);
+                friendFunction(set1, set2);
+                break;
+            case 8:     //exit(work)
                 work = false;
                 break;
             default:

ddaa6f80238a84be4fbb85ecfac1239fbcf36bbd
remoppou <voronezhskij_n_m@sc.vsu.ru>
2022-04-05 12:47:40 +0300

    Friend function

---
+++ cmake-build-debug/CMakeFiles/Task6_var7.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task6_var7.dir/main.cpp.obj"
+  "CMakeFiles/Task6_var7.dir/main.cpp.obj.d"
+  "Task6_var7.exe"
+  "Task6_var7.exe.manifest"
+  "Task6_var7.pdb"
+  "libTask6_var7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Task6_var7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

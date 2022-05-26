8dfdf6b661c3a67bf04cdb257004a616c76444e5
remoppou <voronezhskij_n_m@sc.vsu.ru>
2022-05-26 13:22:26 +0300

    Ready

---
+++ cmake-build-debug/CMakeFiles/Task9.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task9.dir/main.cpp.obj"
+  "CMakeFiles/Task9.dir/main.cpp.obj.d"
+  "Task9.exe"
+  "Task9.exe.manifest"
+  "Task9.pdb"
+  "libTask9.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Task9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -3,69 +3,81 @@
 #include <string>
 #include <set>
 #include <iterator>
+
 using namespace std;
 
-bool falseChars(char a) {
-    if ((a == 32) || (a == 33) || (a == 40) || (a == 41) || (a == 44) || (a == 46) || (a == 63)) {
-        return true;
-    } else return false;
-}
+class Set {
+private:
+    set<string> map;
+    string str;
 
-string readFromFile(string string1, string str) {
-    ifstream in(string1);
-    if(!in.is_open()) {
-        cout << "Error of open file " << endl;
-    } else {
-        while (!in.eof()) {
-            getline(in, str);
-        }
+    bool falseChars(char a) {
+        if ((a == 32) || (a == 33) || (a == 40) || (a == 41) || (a == 44) || (a == 46) || (a == 63)) {
+            return true;
+        } else return false;
     }
-    in.close();
-    return str;
-}
 
-set<string> algorithm(set<string> set1, string& string1) {
-    int length = string1.length();
-    string word;
-    for (int i = 0; i < length; i++) {
-        if (!falseChars(string1[i])) {
-            word += string1[i];
+    string readFromFile(string string1) {
+        ifstream in(string1);
+        if (!in.is_open()) {
+            cout << "Error of open file " << endl;
         } else {
-            if (word != "") {
-                set1.insert(word);
-                word = "";
+            while (!in.eof()) {
+                getline(in, str);
             }
         }
+        in.close();
+        return str;
     }
-    return set1;
+
+public:
+    Set() {};
+
+    set<string> algorithm(string path) {
+        str = readFromFile(path);
+        int length = str.length();
+        string word;
+        for (int i = 0; i < length; i++) {
+            if (!falseChars(str[i])) {
+                word += str[i];
+            } else {
+                if (word != "") {
+                    map.insert(word);
+                    word = "";
+                }
+            }
+        }
+        return map;
+    };
+
+    void output() {
+        copy(map.begin(), map.end(), ostream_iterator<string>(cout, " "));
+    }
+
+    ~Set() {
+        map.clear();
+    }
 };
 
-void output(set<string> set1) {
-    copy(set1.begin(), set1.end(), ostream_iterator<string>(cout, " "));
-}
-
-//D:\C++Projects\Task3_var6\f1.txt
 int main() {
-    string str;
     string path;
     bool check = true;
-    set<string> set;
+    Set mySet;
     while (check) {
         int choose;
         cout << "Choose action(if you want break, enter 3): " << endl;
-        cout << "1. Add text;"<< endl;
-        cout << "2. Output map;"<< endl;
-        cout << "3. Exit;"<< endl;
+        cout << "1. Add text;" << endl;
+        cout << "2. Output map;" << endl;
+        cout << "3. Exit;" << endl;
         cin >> choose;
         if (choose > 3 || choose < 1) {
-            cout <<"False interval!!!"<<endl;
-        } else if (choose == 1){
-            cout << "Enter path to file: "<< endl;
+            cout << "False interval!!!" << endl;
+        } else if (choose == 1) {
+            cout << "Enter path to file: " << endl;
             cin >> path;
-            str = readFromFile(path, str);
-            set = algorithm(set, str);
+            mySet.algorithm(path);
         } else if (choose == 2) {
-            output(set);
+            mySet.output();
             cout << endl;
         } else {
             check = false;
